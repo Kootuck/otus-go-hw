a7edd17738881f6fb114244d184d695bc4b4b348
El Magzhanov <kootuck@proton.me>
2024-04-09 15:07:12 +0200

    HW05 is completed

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,68 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("alternative concurrency check", func(t *testing.T) {
+		// 1. Время выполнения в одного воркера
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 1
+		maxErrorsCount := 1
+
+		start := time.Now()
+		Run(tasks, workersCount, maxErrorsCount)
+		elapsedTimeSingle := time.Since(start)
+
+		// 2. Теперь запускаем конкурентное выполнение
+		runTasksCount = 0
+		workersCount = 5
+
+		// 3. Конкурентное выполнение должно быть быстрее, даже с учетом вызова require ..
+		require.Eventually(t, func() bool {
+			Run(tasks, workersCount, maxErrorsCount)
+			return true
+		}, elapsedTimeSingle, time.Second*1) // "Чистое время" ~0.5с, с учетом обвзяки ~ 1.5с, однопоточно ~ 2.5c
+	})
+
+	// Считаем что когда параметр m(допустимое число ошибок) == 0, то ошибок м.б. сколь угодно много.
+	t.Run("check zero error threshold", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var errorsCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				if rand.Intn(2) == 1 {
+					atomic.AddInt32(&errorsCount, 1)
+					return err
+				}
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 0
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	})
 }
