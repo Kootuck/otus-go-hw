eadda905f4d121c1d277b0a3507e197809c0dcda
El Magzhanov <kootuck@proton.me>
2024-04-12 15:31:15 +0200

    HW06 completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,48 @@ type (
 
 type Stage func(in In) (out Out)
 
+// we are not providing done channel for Stages by task conditions
+// to give our pipeline the ability to be shutdown via done/quit channel
+// we're wrapping 1st and last stages into additional, closeable channels (wrappers).
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	pipelineEntrance := channelWrapper(in, done)
+
+	// assemble pipeline
+	var stageIn In
+	var stageOut Out
+
+	stageIn = pipelineEntrance
+
+	for _, s := range stages {
+		stageOut = s(stageIn)
+		stageIn = stageOut
+	}
+
+	return channelWrapper(stageOut, done)
+}
+
+func channelWrapper(wrappedChannel In, done In) (wrapper Bi) {
+	var msg interface{}
+	ok := true
+	wrapper = make(Bi)
+
+	go func() {
+		defer func() {
+			close(wrapper)
+		}()
+
+		for ok {
+			select {
+			case <-done:
+				return
+			case msg, ok = <-wrappedChannel:
+				if !ok {
+					return
+				}
+				wrapper <- msg
+			}
+		}
+	}()
+
+	return wrapper
 }
