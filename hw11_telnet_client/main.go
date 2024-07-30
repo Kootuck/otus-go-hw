398e7f75ea578469ad789e6207fc92f8073d0218
El Magzhanov <kootuck@proton.me>
2024-07-30 17:52:12 +0200

    HW11 (telnet) completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,97 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type Parameters struct {
+	Host    string
+	Port    string
+	Timeout time.Duration
+}
+
+var p Parameters
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	// 1. Programm arguments
+	err := setParameters()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// 2. Setting up client and tcp connection
+	addr := net.JoinHostPort(p.Host, p.Port)
+	telnet := NewTelnetClient(addr, p.Timeout, os.Stdin, os.Stdout)
+
+	err = telnet.Connect()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	defer telnet.Close()
+
+	// 3. Setting context:
+	// a. Signals-responsive;
+	// b. Cancel: make blocking i/o quit via connection deadline.
+	ctxWCancel, cancel := context.WithCancel(context.Background())
+	ctx, _ := signal.NotifyContext(ctxWCancel, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel() // all paths cancel the context to avoid context leak.
+
+	telnet.SetDeadline(ctx)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// 4. sending messages from client.
+	go func() {
+		defer func() {
+			wg.Done()
+			cancel()
+		}()
+		err := telnet.Send()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		}
+	}()
+
+	// 5. receiving messages.
+	go func() {
+		defer func() {
+			wg.Done()
+			cancel()
+		}()
+		err := telnet.Receive()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		}
+	}()
+
+	wg.Wait()
+}
+
+func setParameters() error {
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "timeout for connection closure if idle (e.g., 5s, 2m, 1h)")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		return fmt.Errorf("invalid arguments, usage: go-telnet [--timeout duration] <host> <port>")
+	}
+
+	p = Parameters{
+		Host:    args[0],
+		Port:    args[1],
+		Timeout: timeout,
+	}
+
+	return nil
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,21 +1,82 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
+
+var ErrFailedToConnect = errors.New("failed to establish a connection")
 
 type TelnetClient interface {
 	Connect() error
 	io.Closer
 	Send() error
 	Receive() error
+	SetDeadline(context.Context)
+}
+type TelnetClientImpl struct {
+	address string
+	timeout time.Duration
+	conn    net.Conn
+	in      io.Reader
+	out     io.Writer
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+func NewTelnetClient(address string, timeout time.Duration, in io.Reader, out io.Writer) TelnetClient {
+	return &TelnetClientImpl{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *TelnetClientImpl) Connect() error {
+	var err error
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		wrapped := fmt.Errorf("net: %w", err)
+		return fmt.Errorf("%w: %v", ErrFailedToConnect, wrapped) //nolint:errorlint
+	}
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *TelnetClientImpl) Close() error {
+	if t.conn != nil {
+		err := t.conn.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close connection: %w", err)
+		}
+		fmt.Println("Connection closed (client)")
+	}
+	return nil
+}
+
+func (t *TelnetClientImpl) Send() error {
+	_, err := io.Copy(t.conn, t.in)
+	if err != nil {
+		return fmt.Errorf("error copying from stdin to conn: %w", err)
+	}
+	return nil
+}
+
+func (t *TelnetClientImpl) Receive() error {
+	_, err := io.Copy(t.out, t.conn)
+	if err != nil {
+		return fmt.Errorf("error copying from conn to stdout: %w", err)
+	}
+	return nil
+}
+
+func (t *TelnetClientImpl) SetDeadline(ctx context.Context) {
+	go func() {
+		<-ctx.Done()
+		if t.conn != nil {
+			t.conn.SetDeadline(time.Now()) // Forces io.Copy to return.
+		}
+	}()
+}
